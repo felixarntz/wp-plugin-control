e288b3077953b7a20bafb1f0d6543bb10e6894a7
Felix Arntz <felix-arntz@t-online.de>
2016-11-16 11:22:47 +0100

    handle plugin toggle links in one function

--- wp-plugin-control/admin/network-plugins-list-table.php
+++ wp-plugin-control/admin/network-plugins-list-table.php
@@ -19,44 +19,40 @@ function wppc_filter_plugins( $plugins ) {
 }
 add_filter( 'all_plugins', 'wppc_filter_plugins' );
 
-function wppc_add_site_toggle_link( $actions, $plugin_file, $plugin_data, $context ) {
+function wppc_add_toggle_link( $actions, $plugin_file, $plugin_data, $context ) {
 	global $page, $s;
 
 	if ( ! current_user_can( 'toggle_plugins' ) ) {
 		return $actions;
 	}
 
-	$disabled_plugins = wppc_get_all_plugins_disabled_for_site();
+	if ( is_network_admin() ) {
+		$disabled_plugins = wppc_get_all_plugins_disabled_for_network();
+
+		$enable_text        = _x( 'Network Enable', 'plugin', 'wp-plugin-control' );
+		$disable_text       = _x( 'Network Disable', 'plugin', 'wp-plugin-control' );
+		/* translators: %s: plugin name */
+		$enable_aria_label  = _x( 'Network Enable %s', 'plugin', 'wp-plugin-control' );
+		/* translators: %s: plugin name */
+		$disable_aria_label = _x( 'Network Disable %s', 'plugin', 'wp-plugin-control' );
+	} else {
+		$disabled_plugins = wppc_get_all_plugins_disabled_for_site();
+
+		$enable_text        = _x( 'Enable', 'plugin', 'wp-plugin-control' );
+		$disable_text       = _x( 'Disable', 'plugin', 'wp-plugin-control' );
+		/* translators: %s: plugin name */
+		$enable_aria_label  = _x( 'Enable %s', 'plugin', 'wp-plugin-control' );
+		/* translators: %s: plugin name */
+		$disable_aria_label = _x( 'Disable %s', 'plugin', 'wp-plugin-control' );
+	}
 
 	if ( in_array( $plugin_file, $disabled_plugins, true ) ) {
-		/* translators: %s: plugin name */
-		$actions['enable'] = '<a href="' . wp_nonce_url( 'plugins.php?action=enable&amp;plugin=' . $plugin_file . '&amp;plugin_status=' . $context . '&amp;paged=' . $page . '&amp;s=' . $s, 'enable-plugin_' . $plugin_file ) . '" class="edit" aria-label="' . esc_attr( sprintf( _x( 'Enable %s', 'plugin', 'wp-plugin-control' ), $plugin_data['Name'] ) ) . '">' . _x( 'Enable', 'plugin', 'wp-plugin-control' ) . '</a>';
+		$actions['enable'] = '<a href="' . wp_nonce_url( 'plugins.php?action=enable&amp;plugin=' . $plugin_file . '&amp;plugin_status=' . $context . '&amp;paged=' . $page . '&amp;s=' . $s, 'enable-plugin_' . $plugin_file ) . '" class="edit" aria-label="' . esc_attr( sprintf( $enable_aria_label, $plugin_data['Name'] ) ) . '">' . $enable_text . '</a>';
 	} else {
-		/* translators: %s: plugin name */
-		$actions['enable'] = '<a href="' . wp_nonce_url( 'plugins.php?action=disable&amp;plugin=' . $plugin_file . '&amp;plugin_status=' . $context . '&amp;paged=' . $page . '&amp;s=' . $s, 'disable-plugin_' . $plugin_file ) . '" class="edit" aria-label="' . esc_attr( sprintf( _x( 'Disable %s', 'plugin', 'wp-plugin-control' ), $plugin_data['Name'] ) ) . '">' . _x( 'Disable', 'plugin', 'wp-plugin-control' ) . '</a>';
+		$actions['disable'] = '<a href="' . wp_nonce_url( 'plugins.php?action=disable&amp;plugin=' . $plugin_file . '&amp;plugin_status=' . $context . '&amp;paged=' . $page . '&amp;s=' . $s, 'disable-plugin_' . $plugin_file ) . '" class="edit" aria-label="' . esc_attr( sprintf( $disable_aria_label, $plugin_data['Name'] ) ) . '">' . $disable_text . '</a>';
 	}
 
 	return $actions;
 }
-add_filter( 'plugin_action_links', 'wppc_add_site_toggle_link', 10, 4 );
-
-function wppc_add_network_toggle_link( $actions, $plugin_file, $plugin_data, $context ) {
-	global $page, $s;
-
-	if ( ! current_user_can( 'toggle_plugins' ) ) {
-		return $actions;
-	}
-
-	$disabled_plugins = wppc_get_all_plugins_disabled_for_network();
-
-	if ( in_array( $plugin_file, $disabled_plugins, true ) ) {
-		/* translators: %s: plugin name */
-		$actions['enable'] = '<a href="' . wp_nonce_url( 'plugins.php?action=enable&amp;plugin=' . $plugin_file . '&amp;plugin_status=' . $context . '&amp;paged=' . $page . '&amp;s=' . $s, 'enable-plugin_' . $plugin_file ) . '" class="edit" aria-label="' . esc_attr( sprintf( _x( 'Network Enable %s', 'plugin', 'wp-plugin-control' ), $plugin_data['Name'] ) ) . '">' . _x( 'Network Enable', 'plugin', 'wp-plugin-control' ) . '</a>';
-	} else {
-		/* translators: %s: plugin name */
-		$actions['enable'] = '<a href="' . wp_nonce_url( 'plugins.php?action=disable&amp;plugin=' . $plugin_file . '&amp;plugin_status=' . $context . '&amp;paged=' . $page . '&amp;s=' . $s, 'disable-plugin_' . $plugin_file ) . '" class="edit" aria-label="' . esc_attr( sprintf( _x( 'Network Disable %s', 'plugin', 'wp-plugin-control' ), $plugin_data['Name'] ) ) . '">' . _x( 'Network Disable', 'plugin', 'wp-plugin-control' ) . '</a>';
-	}
-
-	return $actions;
-}
-add_filter( 'network_admin_plugin_action_links', 'wppc_add_network_toggle_link', 10, 4 );
+add_filter( 'plugin_action_links', 'wppc_add_toggle_link', 10, 4 );
+add_filter( 'network_admin_plugin_action_links', 'wppc_add_toggle_link', 10, 4 );
